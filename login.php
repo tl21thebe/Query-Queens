f9a622fd6e1943c657928161c23b8cc9fc1c09ef
diditlaka <diditlaka51@gmail.com>
2025-05-14 02:04:11 +0200

    added styling to index.html and created a base structure for the other files
    i changed the css styling of the index.html and added a navbar for the pages that we can direct , also created a base structure for the other files as well

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+include 'header.php';
+?>
+
+<link rel = "stylesheet" href = "login.css">
+
+<main>
+    <h2>Login</h2>
+    <?php if(isset($_SESSION['user'])): ?>
+         <p>You are already logged in. You can now:</p>
+    <?php else: ?>
+        <form method="post" id="loginForm">
+            <label for="email">Email:</label><br>
+            <input type="email" id="email" name="email" required><br>
+
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" required><br><br>
+
+            <button type="submit">Login</button>
+            <div id="loginMessage"></div>
+        </form>
+    <?php endif; ?>
+</main>
---
+++ signup.php
@@ -0,0 +1,42 @@
+<?php
+include 'header.php'; 
+?>
+
+<!-- Add the specific CSS for this page -->
+<link rel="stylesheet" href="signup.css">
+
+<div class="signup-container">
+    <h2>Create an Account</h2>
+    <div id="error-message" class="error-message"></div>
+    <form id="signup-form" class="signup-form">
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+            <span class="error-text" id="name-error"></span>
+        </div>
+        <div class="form-group">
+            <label for="surname">Surname:</label>
+            <input type="text" id="surname" name="surname" required>
+            <span class="error-text" id="surname-error"></span>
+        </div>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <span class="error-text" id="email-error"></span>
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <span class="error-text" id="password-error"></span>
+        </div>
+        <div class="form-group">
+            <label for="confirm_password">Confirm Password:</label>
+            <input type="password" id="confirm_password" name="confirm_password" required>
+            <span class="error-text" id="confirm-password-error"></span>
+        </div>
+        <div class="form-group">
+            <button type="submit" id="register-btn">Register</button>
+        </div>
+        <p>Already have an account? <a href="login.php">Login here</a></p>
+    </form>
+</div>
