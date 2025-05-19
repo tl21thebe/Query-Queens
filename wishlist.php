dade2f8cb06ab13cc015ad7d3d47d610a0c5da1b
diditlaka <diditlaka51@gmail.com>
2025-05-19 15:29:20 +0200

    page layout for wishlist

---
+++ wishlist.php
@@ -0,0 +1,18 @@
+<?php
+include 'header.php'; 
+include 'footer.php'
+?>
+
+<!-- Add the specific CSS for this page -->
+<link rel="stylesheet" href="wishlist.css">
+
+<h1 class="wishlist-title">Your Wishlist</h1>
+
+<div class="wishlist-container">
+    <!-- Products will be added dynamically here from wishlist.js -->
+</div>
+
+<!-- Loader -->
+<div class="loader-container" style="display: none;">
+
+<script src="wishlist.js"></script>
