b049b1b6b66b860c3b9abb84b6bb194a7652fb33
diditlaka <diditlaka51@gmail.com>
2025-05-20 19:58:09 +0200

    moved into their respective folders

--- php/config.php
+++ php/config.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 okay here is what we decided on , i think T menitoned that we are no longer going to use mock data anymore and the api that they provided
 but now we are going to create our own products based on the theme , we decided on shoes
@@ -21,46 +20,30 @@ Sabira mentioned that we can use her details for her wheatley and her php Admin 
 
 okay bye :) 
 */
+if(session_status() == PHP_SESSION_NONE)//create a session that can be included in the header.php which is included in every other file so that we dont need to start the session at the beginning of evry file 
+{
+    session_start();
+}
 
 $host = 'localhost'; // This works ON wheatley. Don't change to 'wheatley.cs.up.ac.za'
 $dbname = 'u23591481_queryqueens_compareIt'; // Your DB name (must start with your student number)
 $user = 'u23591481'; // Your student number
 $pass = 'YFOQ5R4KUUEFVGAZR2FQDGWRJT6LE5Z3'; // Find this in the password file on Wheatley
 
-try {
+try 
+{
 
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+} 
+catch (PDOException $e) 
+{
     die(json_encode([
         "status" => "error",
         "timestamp" => time() * 1000,
         "data" => "Database connection failed: " . $e->getMessage()
     ]));
 }
-/*if(session_status() == PHP_SESSION_NONE)
-{
-    session_start();
-}
-
-$host = 'localhost';
-$dbname = 'compareIt';
-$username = '';
-$password = '';
-
-try 
-{
-    // Try using the mysql_native_password authentication method
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, 
-    [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'"
-    ]);
-} 
-catch (PDOException $e) 
-{
-    die("Connection failed: " . $e->getMessage());
-}*/
 ?>
 
 
