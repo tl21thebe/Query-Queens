a6150e7fb57bf88a59d3c07f947628e05a33f414
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-05-21 10:42:40 +0200

    Create view.php
    view page for specific product details

---
+++ php/view.php
@@ -0,0 +1,18 @@
+<?php include('php/header.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>View Product</title>
+</head>
+<body>
+  <div id="product-details">
+    <p>Loading product...</p>
+  </div>
+
+  <script type="module" src="js/view.js"></script>
+</body>
+</html>
+
+<?php include('php/footer.php'); ?>
