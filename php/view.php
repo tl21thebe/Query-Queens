9a89970b1685a91bc042cc5719b8e33e89b3d2a0
diditlaka <diditlaka51@gmail.com>
2025-05-21 11:05:22 +0200

    added the view page and then fixed the link for the header.php for the styling

--- php/view.php
+++ php/view.php
@@ -1,18 +1,24 @@
-<?php include('php/header.php'); ?>
+<?php include 'header.php'; 
+?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>View Product</title>
-</head>
-<body>
-  <div id="product-details">
-    <p>Loading product...</p>
-  </div>
+<!-- Add the specific CSS for this page -->
+<link rel="stylesheet" href="../css/view.css">
 
-  <script type="module" src="js/view.js"></script>
-</body>
-</html>
+<section class="product-view">
+    <div class="carousel">
+        <img id="main-image" src="" alt="Product Image">
+        <div class="image-thumbnails"></div>
+    </div>
+    
+    <div class="product-details">
+        <h1 id="product-title"></h1>
+        <p class="price" id="product-price"></p>
+        <p class="description" id="product-description"></p>
+        
+        <ul class="extra-features" id="extra-features"></ul>
+    </div>
+</section>
 
-<?php include('php/footer.php'); ?>
+<script src="../js/view.js"></script>
+
+<?php include_once 'footer.php'; ?>
