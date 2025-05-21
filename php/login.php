de13d06aad25086b80d45b0d710b4d2a0e84b137
GiftMHB <giftmohuba159@gmail.com>
2025-05-21 10:17:03 +0200

    few changes

--- php/login.php
+++ php/login.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - CompareIt</title>
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+
 <div class="container">
     <div class="row">
         <div class="form-container">
@@ -19,3 +29,6 @@
         </div>
     </div>
 </div>
+
+</body>
+</html>
