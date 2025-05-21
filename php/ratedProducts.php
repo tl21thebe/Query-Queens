9cabf77fb1394afecd5fe3f35574315a878a0a91
GiftMHB <giftmohuba159@gmail.com>
2025-05-21 11:40:33 +0200

    added ratedProducts.css

--- php/ratedProducts.php
+++ php/ratedProducts.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Rated Products</title>
-    <link rel="stylesheet" href="../css/products.css">
+    <link rel="stylesheet" href="../css/ratedProducts.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
