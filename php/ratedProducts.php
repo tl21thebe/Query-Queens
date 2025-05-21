29ed3d92536f1d96e5746c1bfa420b69ef4a397c
GiftMHB <giftmohuba159@gmail.com>
2025-05-21 12:58:18 +0200

    Update ratedProducts.php
    Added a link

--- php/ratedProducts.php
+++ php/ratedProducts.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Rated Products</title>
-    <link rel="stylesheet" href="../css/ratedProducts.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/ratedProducts.css">
+    <script src="../js/ratedProducts.js" defer></script>
 </head>
 <body>
 
