c52b40866a9d64f6680b4c632c86ebfcee599f2b
diditlaka <diditlaka51@gmail.com>
2025-05-26 11:02:04 +0200

    changed the path of the config.php

--- sabiraV_api.php
+++ sabiraV_api.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-require_once 'config.php';
+require_once 'php/config.php';
 
 
 if (session_status() == PHP_SESSION_NONE) {
